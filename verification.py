6b2a7b1282f99e3deebf69565957e5314b796fbb
moYdaDyr <113461028+moYdaDyr@users.noreply.github.com>
2023-02-01 20:35:48 +0500

    Update verification.py

--- verification.py
+++ verification.py
@@ -1,22 +1,22 @@
 import random
 import smtplib
 
-def SendVerificationCode(toaddrs):
+def SendVerificationCode(toAddrs):
     
     email = 'hseloveperm.bot@yandex.ru'
-    VerCode = str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
+    verCode = str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
 
     subject = 'Верификация'
-    message = VerCode
+    message = 'placeholder ' + verCode + ' placeholder'
     
     msg = 'From: {}\r\nTo: {}\r\nSubject: {}\n\n{}'.format(
-       email, toaddrs, subject, message
+       email, toAddrs, subject, message
     )
  
     server = smtplib.SMTP('smtp.yandex.ru')
     server.starttls()
     server.login(email, 'phoenix14bot')
-    server.sendmail(email, toaddrs, msg.encode('utf8'))
+    server.sendmail(email, toAddrs, msg.encode('utf8'))
     server.quit()
 	
-    return VerCode
+    return verCode
